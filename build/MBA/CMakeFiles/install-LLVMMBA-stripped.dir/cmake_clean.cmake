7c530eba0fa03471ad0acc9ee26063c8c30e4012
Li, Chia-Chang <hmtp42563@gmail.com>
2022-10-08 15:27:14 +0800

    Finish cmake infra and update .gitignore

--- build/MBA/CMakeFiles/install-LLVMMBA-stripped.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-LLVMMBA-stripped"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-LLVMMBA-stripped.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
